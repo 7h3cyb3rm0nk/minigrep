c806ab1886fc90875f28891b922f4bdaa02192da
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2023-08-31 02:04:19 +0530

    Add init files Added initial files, a basic program to access the command line arguments

---
+++ minigrep/target/debug/deps/minigrep-6b05245b4e5b0382.d
@@ -0,0 +1,5 @@
+/home/ramees/prog/rust/minigrep/minigrep/target/debug/deps/minigrep-6b05245b4e5b0382: src/main.rs
+
+/home/ramees/prog/rust/minigrep/minigrep/target/debug/deps/minigrep-6b05245b4e5b0382.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/ramees/prog/rust/minigrep/minigrep/target/debug/minigrep: /home/ramees/prog/rust/minigrep/minigrep/src/main.rs
