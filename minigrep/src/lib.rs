471d114721c672aae6a13ffc84eb8301e37ad505
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2023-09-01 22:40:43 +0530

    Add case_insensitive_search() added seperate function for case insensitive search using env variables

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
 use std::fs;
-
+use std::env;
 pub struct Config {
    pub query: String,
    pub file_path: String,
+   pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,8 +15,9 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config{ query, file_path })
+        Ok(Config{ query, file_path, ignore_case, })
 
     }
 }
@@ -24,7 +26,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    }
+    else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -37,7 +46,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result()
+    fn case_sensitive()
     {
         let query = "duct";
         let contents = "\
@@ -47,7 +56,33 @@ Pick three,";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+    #[test]
+    fn case_insensitive() {
+        let query = "dUcT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three,";
+
+        assert_eq!(vec!["safe, fast, productive."], search_case_insensitive(query, contents));
+    }
 }
+
+pub fn search_case_insensitive<'a>
+(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+        
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
--- minigrep/target/debug/minigrep.d
+++ minigrep/target/debug/minigrep.d
@@ -1,1 +1,1 @@
-/home/ramees/prog/rust/minigrep/minigrep/target/debug/minigrep: /home/ramees/prog/rust/minigrep/minigrep/src/main.rs
+/home/ramees/prog/rust/minigrep/minigrep/target/debug/minigrep: /home/ramees/prog/rust/minigrep/minigrep/src/lib.rs /home/ramees/prog/rust/minigrep/minigrep/src/main.rs
